6f2dabe774310ba46385cfe5e4028fa046b77713
Minh Nguyen <vn.ndminh@gmail.com>
2022-03-05 22:14:27 +0000

    Add input file, read input file to store matrix

--- HW1/Minh_Nguyen.cpp
+++ HW1/Minh_Nguyen.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <string>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <cstdio>
-#include <string>
 
 using namespace std;
 
 int main () {
     int fd1[2];     // Store two ends of the first pipe
     int fd2[2];     // Store two ends of the second pipe
+
+    // Read adjacency matrix from input file
+    int input_arr[7][7];      // Two dimensions array to store input from files
+    
+    ifstream inputFile ("input.txt");
+    string line;
+    if (inputFile.is_open()) {
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                inputFile >> input_arr[i][j];
+            }
+        }
+        inputFile.close();
+    }
 
     pid_t p;
     
@@ -19,6 +35,7 @@ int main () {
     }
 
     p = fork();
+
     if (p < 0) {
         perror("Fork failed");
         return 1;
@@ -31,7 +48,7 @@ int main () {
     
     // Child process
     else {
-        
+
     }
 
     return 0;
