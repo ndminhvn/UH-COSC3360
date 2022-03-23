830738c741f0a3bfd3d2ef1c3655a8dfb70cad1f
Minh Nguyen <vn.ndminh@gmail.com>
2022-03-22 22:24:21 -0500

    Initialize HW2

---
+++ HW2/Minh_Nguyen.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
